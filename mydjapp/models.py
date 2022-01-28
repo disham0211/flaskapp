3e67ce2da58f614cbeef8b4bd80186277f0c18ff
Mahendra Kumar <disha.mahendrakumar@dxc.com>
2022-01-28 17:08:35 +0530

    added mydjappproject

---
+++ mydjapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Register(models.Model):
+	regid = models.AutoField(primary_key=True)
+	name=models.CharField(max_length=50)
+	username=models.CharField(max_length=50,unique=True)
+	password=models.CharField(max_length=10)
+	mobile=models.CharField(max_length=10)	
+	address=models.CharField(max_length=1000)
+	city=models.CharField(max_length=10)
+	gender=models.CharField(max_length=20)
+	info=models.CharField(max_length=1000)
